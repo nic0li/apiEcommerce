be509ac4434d4f17fec25d5b822ce28656075ad9
Nicoli <nicoli1992@gmail.com>
2021-03-28 12:34:43 -0300

    adicionando estrutura inicial spring boot

---
+++ apiEcommerce/src/test/java/org/generation/apiEcommerce/ApiEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.apiEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
